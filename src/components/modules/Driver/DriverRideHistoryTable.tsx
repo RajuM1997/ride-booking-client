2d7c034fea90624604829f435ba99c8008893d33
RajuM1997 <mraju5797@gmail.com>
2025-11-07 20:43:15 +0600

    filter option added Date: 7-11-2025

--- src/components/modules/Driver/DriverRideHistoryTable.tsx
+++ src/components/modules/Driver/DriverRideHistoryTable.tsx
@@ -15,23 +15,181 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import { useGetDriverRideQuery } from "@/redux/features/driver/driver.api";
 import type { IRide } from "@/types";
 import { format } from "date-fns";
 import { useState } from "react";
-import { RideFilterOption } from "./RideFilterOption";
-import { Card } from "@/components/ui/card";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+// import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { cn } from "@/lib/utils";
+
+const searchSchema = z.object({
+  pickup: z.string().optional(),
+  destination: z.string().optional(),
+  date: z.date().optional(),
+});
 
 const DriverRideHistoryTable = () => {
   const [currentPage, setCurrentPage] = useState(1);
-  const { data } = useGetDriverRideQuery({ page: currentPage, limit: 1 });
-  const totalPage = data?.meta?.totalPage || 1;
+  const form = useForm<z.infer<typeof searchSchema>>({
+    resolver: zodResolver(searchSchema),
+    defaultValues: {
+      pickup: "",
+      destination: "",
+      date: new Date(),
+    },
+  });
+  const [date, setDate] = useState<string | undefined>();
+  const [pickup, setPickup] = useState<string | undefined>();
+  const [destination, setDestination] = useState<string | undefined>(undefined);
+  const [clearSearch, setClearSearch] = useState(false);
+  const { data } = useGetDriverRideQuery({
+    page: currentPage,
+    limit: 1,
+    pickup,
+    destination,
+    createdAt: date,
+  });
+
+  const handleSearch = async (data: z.infer<typeof searchSchema>) => {
+    if (data.date) {
+      const newDate = data.date;
+      setDate(newDate.toISOString());
+    }
+    if (data.pickup) {
+      setPickup(data.pickup);
+    }
+    if (data.destination) {
+      setDestination(data.destination);
+    }
+    setClearSearch(true);
+  };
+
+  const handleClearSearch = () => {
+    setDate(undefined);
+    setPickup(undefined);
+    setDestination(undefined);
+    setClearSearch(false);
+    form.reset();
+  };
   console.log(data);
+
+  const totalPage = data?.meta?.totalPage || 1;
 
   return (
     <div className="w-full max-w-7xl mx-auto">
       <Card className="p-5">
-        <RideFilterOption />
+        <Form {...form}>
+          <form id="search-form" onSubmit={form.handleSubmit(handleSearch)}>
+            <div className="flex flex-col gap-3">
+              <div className="flex w-full justify-end flex-col gap-6">
+                <CardContent className="grid grid-cols-1 md:items-end md:grid-cols-4 gap-6 pt-3">
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="pickup"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Pickup</FormLabel>
+                          <FormControl>
+                            <Input placeholder="pickup" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="destination"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Destination</FormLabel>
+                          <FormControl>
+                            <Input placeholder="destination" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="grid gap-3">
+                    <FormField
+                      control={form.control}
+                      name="date"
+                      render={({ field }) => (
+                        <FormItem className="flex flex-col flex-1">
+                          <FormLabel>Select A Date</FormLabel>
+                          <Popover>
+                            <PopoverTrigger asChild>
+                              <FormControl>
+                                <Button
+                                  variant={"outline"}
+                                  className={cn(
+                                    "w-full pl-3 text-left font-normal",
+                                    !field.value && "text-muted-foreground"
+                                  )}
+                                >
+                                  {field.value ? (
+                                    format(field.value, "PPP")
+                                  ) : (
+                                    <span>Pick a date</span>
+                                  )}
+                                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                              </FormControl>
+                            </PopoverTrigger>
+                            <PopoverContent
+                              className="w-auto p-0"
+                              align="start"
+                            >
+                              <Calendar
+                                mode="single"
+                                onSelect={field.onChange}
+                                // onDayBlur={field.onBlur}
+                                captionLayout="dropdown"
+                              />
+                            </PopoverContent>
+                          </Popover>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                  <div className="flex gap-3 ">
+                    <Button form="search-form">Search</Button>
+                    {clearSearch && (
+                      <Button onClick={handleClearSearch}>Clear</Button>
+                    )}
+                  </div>
+                </CardContent>
+              </div>
+            </div>
+          </form>
+        </Form>
       </Card>
       <Table>
         <TableHeader>
@@ -46,7 +204,7 @@ const DriverRideHistoryTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          {data?.data?.length &&
+          {data?.data?.length > 0 &&
             data?.data?.map((ride: IRide, i: number) => (
               <TableRow key={ride._id}>
                 <TableCell className="font-medium">{i + 1}</TableCell>
@@ -64,6 +222,7 @@ const DriverRideHistoryTable = () => {
             ))}
         </TableBody>
       </Table>
+
       {totalPage > 1 && (
         <div className="flex justify-center mt-4">
           <div>
